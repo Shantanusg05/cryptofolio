0832a47f5ffbaff030cfb051a78f4c1e06e3b171
Shantanusg05 <161843863+Shantanusg05@users.noreply.github.com>
2026-03-30 20:00:45 +0530

    Remove database connection code
    Remove MongoDB connection logic from db.js

--- db.js
+++ db.js
@@ -1,16 +1,1 @@
-const { MongoClient } = require('mongodb');
 
-let client;
-let db;
-
-async function connectDB() {
-    if (!client) {
-        client = new MongoClient(process.env.MONGODB_URL);
-        await client.connect();
-        db = client.db("cryptofolio");
-        console.log("MongoDB Connected");
-    }
-    return db;
-}
-
-module.exports = connectDB;
